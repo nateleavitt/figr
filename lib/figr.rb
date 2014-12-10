e462bd6a84aa6278286cdc6af9bd9d3d4753739c
Nathan Leavitt <nateleavitt@gmail.com>
2014-12-10 15:10:29 -0700

    making changes to cli structure

--- lib/figr.rb
+++ lib/figr.rb
@@ -2,18 +2,6 @@ require "figr/version"
 
 module Figr
 
-  module CLI
-
-
-    extend self
-
-    def run(*args)
-      command = args.shift.strip rescue "help"
-      FIGR::Command.load
-      FIGR::Command.run(command, args)
-      puts "Here is the command: #{command} and opts: #{args.inspect}"
-    end
-
     def login
       puts "We need your GitHub login to identify you. \n
             This information will not be sent to Figr, only to GitHub. \n
@@ -23,32 +11,5 @@ module Figr
     def say_hello
       puts "This is figr. Coming in lour and clear. Over."
     end
-
-    def test(opts)
-      opts.each do |o|
-        puts o
-      end
-    end
-
-    def set_val(val)
-      puts "Your value of #{val} has been set"
-    end
-
-    def get_val(val)
-      puts "Here is your value: #{val}"
-    end
-
-    private
-    # can't use flatten as it will flatten hashes
-      def preparse(unparsed, args = [], opts = {})
-        case unparsed
-        when Hash  then opts.merge! unparsed
-        when Array then unparsed.each { |e| preparse(e, args, opts) }
-        else args << unparsed.to_s
-        end
-        [args, opts]
-      end
-
-  end
 
 end
--- lib/figr/cli.rb
+++ lib/figr/cli.rb
@@ -1,8 +1,10 @@
 class Figr::CLI
 
   def self.run(*args)
-     # parse command and options and run them
-
+    command = args.shift.strip rescue "help"
+    FIGR::Command.load
+    FIGR::Command.run(command, args)
+    puts "Here is the command: #{command} and opts: #{args.inspect}"
   end
 
 end
